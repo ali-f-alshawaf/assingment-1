d8903ac1de88ac023fcc68b9ae91e7d08f2d41d1
ali84sama <ali.f.alshawaf@gmail.com>
2020-05-04 09:23:20 +0300

    Initial Commit

---
+++ Preyer Time/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Preyer Time
+//
+//  Created by ALI on 5/4/20.
+//  Copyright © 2020 ALI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
