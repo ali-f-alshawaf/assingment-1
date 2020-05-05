b44a376efac7ef6ade205ccfa1e5274ee953ce0e
ali84sama <ali.f.alshawaf@gmail.com>
2020-05-05 11:40:01 +0300

    calculator Done

--- Preyer Time/Calculator.swift
+++ Preyer Time/Calculator.swift
@@ -22,13 +22,248 @@ struct Calculator: View {
                 .padding(.leading, 231.0)
                 .offset(y:-180)
             
-            HStack{
-            Text("%")
+            HStack(spacing: 10){
+                
+            
+            Text("C")
+                .font(.system(size: 40))
                 .bold()
                 .foregroundColor(.white)
-                .background(Color(#colorLiteral(red: 0.9945489764, green: 0.6004671454, blue: 0.01180825382, alpha: 1)))
+                .padding()
+                .multilineTextAlignment(.center)
+                .padding()
+                .background(Color.gray)
+                .clipShape(Circle())
                 
-                }
+                
+                Text("±")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("%")
+                    .font(.system(size: 30))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("÷")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.orange)
+                    .clipShape(Circle())
+                
+            
+            }
+            .padding(.all, 3.0)
+            .offset(y:-90)
+            
+            
+            HStack(spacing: 10){
+                
+                
+                Text("7")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                
+                Text("8")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("9")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("X")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.orange)
+                    .clipShape(Circle())
+                
+                
+            }
+            .padding(.all, 3.0)
+            .offset(y:10)
+            
+            HStack(spacing: 10){
+                
+                
+                Text("4")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                
+                Text("5")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("6")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("-")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.orange)
+                    .clipShape(Circle())
+                
+                
+            }
+            .padding(.all, 3.0)
+            .offset(y:110)
+            
+            
+            HStack(spacing: 10){
+                
+                
+                Text("1")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                
+                Text("2")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("3")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                
+                Text("+")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .background(Color.orange)
+                    .clipShape(Circle())
+                
+                
+            }
+            .padding(.all, 3.0)
+            .offset(y:210)
+            
+            
+               
+               HStack(spacing: 10){
+                   
+                   
+                   Text("0")
+                       .bold()
+                       .foregroundColor(.white)
+                    .padding(.horizontal, 55.0)
+                       .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                       .background(Color.gray)
+                    .font(.system(size: 70))
+                    .clipShape(Capsule())
+                   
+                   
+                
+                   
+                   Text(".")
+                       .font(.system(size: 80))
+                       .bold()
+                       .foregroundColor(.white)
+                       .padding()
+                       .multilineTextAlignment(.center)
+                       .padding()
+                       .background(Color.gray)
+                       .clipShape(Circle())
+                   
+                   Text("-")
+                       .font(.system(size: 50))
+                       .bold()
+                       .foregroundColor(.white)
+                       .padding()
+                       .multilineTextAlignment(.center)
+                       .padding()
+                       .background(Color.orange)
+                       .clipShape(Circle())
+                   
+                   
+               }
+               .padding(.all, 3.0)
+               .offset(y:310)
+            
+        
             }
         }
     }
